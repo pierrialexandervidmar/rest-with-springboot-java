302cf2f1faa99439a2e1c2d088c334063ddb7979
pierrialexandervidmar <pierri@hotmail.com.br>
2024-08-01 20:37:41 -0300

    Ajustes no controller

--- 07_IntegrationDBMySql/rest-with-springboot-java/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07_IntegrationDBMySql/rest-with-springboot-java/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,6 +1,7 @@
 package br.com.erudio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,26 +18,31 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public List<Person> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public Person findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public Person create(@RequestBody Person person) {
         return service.create(person);
     }
 
     @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
     public Person update(@RequestBody Person person) {
         return service.update(person);
     }
 
-    @DeleteMapping()
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
     }
